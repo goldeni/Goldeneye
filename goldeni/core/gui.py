18215ddbde03f1b6444dce67e0fb03464bd250a5
goldeni <mtmasset@mtu.edu>
2011-10-17 05:16:24 -0400

    Done with experimental branch

--- goldeni/core/gui.py
+++ goldeni/core/gui.py
@@ -2,6 +2,7 @@
 
 import Tkinter
 import Image,ImageTk,tkFileDialog,sys,tkMessageBox
+import main
 
 ####### Definition buttons CMDs ###########
 
@@ -57,12 +58,12 @@ class mainWindow(Tkinter.Tk):
 
 		self.title("Iris Processing")
 
-		newimage = Tkinter.Label(self, image=loadImage)
-		newimage.loadImage=loadImage
-		newimage.grid(row=0,column=0,rowspan=2,columnspan=2)
+		self.newimage = Tkinter.Label(self, image=loadImage)
+		self.newimage.loadImage=loadImage
+		self.newimage.grid(row=0,column=0,rowspan=2,columnspan=2)
 
-		button = Tkinter.Button(self, text="Process Image",command=self.processImage,bg="white")
-		button.grid(row=2,column=0,columnspan=2)
+		self.button = Tkinter.Button(self, text="Process Image",command=lambda i=imgPath: self.processImage(i) ,bg="white")
+		self.button.grid(row=2,column=0,columnspan=2)
 
 	def paraSubmit(self):
 		tkMessageBox.showinfo("Iris Processing", "Test")
@@ -71,7 +72,14 @@ class mainWindow(Tkinter.Tk):
 		tkMessageBox.showinfo("SEARCHING...", "RECORD")
 
 	def processImage(self,path):
-		tkMessageBox.showinfo("Iris Processing", "Test")
+		self.newimage.destroy()
+		self.title("Iris Processed")
+		self.button.destroy()
+
+		loadImage = ImageTk.PhotoImage(main.main(path))
+		self.newimage = Tkinter.Label(self, image=loadImage)
+		self.newimage.loadImage=loadImage
+		self.newimage.grid(row=0,column=0,rowspan=2,columnspan=2)
 
 	def initialize(self):
 		self.grid()
@@ -79,6 +87,9 @@ class mainWindow(Tkinter.Tk):
 		self.backImage = ImageTk.PhotoImage(file="images/1.gif")
 		self.background = Tkinter.Label(self,image=self.backImage,bg="black")
 		self.background.grid(row=0,column=0,columnspan=2,rowspan=2)
+
+		self.grid_columnconfigure(0,weight=1)
+		self.resizable(True,False)
 
 		menubar=Tkinter.Menu(self)
 		filemenu = Tkinter.Menu(menubar,tearoff=0)
@@ -105,123 +116,4 @@ if __name__ == "__main__":
 	root = mainWindow(None)
 	root.title("Healthcare Iris Biometric Scanner")
 	root.mainloop()
-
-
-#########################
-if False:
-	def globalPath(path):
-		return path
-
-	def loadImage():
-		imgPath = tkFileDialog.askopenfilename()
-		loadImage = ImageTk.PhotoImage(file=imgPath)
-		print imgPath,loadImage
-
-		w.destroy()
-
-		limage = Toplevel()
-		limage.title("Iris Processing")
-		limage.geometry('250x250+0+0')
-
-		newimage = Label(limage, image=loadImage)
-		newimage.loadImage=loadImage
-		newimage.pack()
-
-		button = Button(limage, text="Process Image",command=processImage,bg="white")
-		button.pack(side = BOTTOM)
-
-	def processImage():
-		tkMessageBox.showinfo("Iris Processing", "Test")
-
-	def changePara():
-		cpara = Toplevel(bg="white")
-		cpara.title("Change Parameters")
-		cpara.geometry('250x250+0+0')
-
-		L1 = Label(cpara, text="Hamming Distance:",bg="white")
-		L1.pack()
-		E1 = Entry(cpara, bd=2)
-		E1.pack()
-
-		L2 = Label(cpara, text="Parameter 2:",bg="white")
-		L2.pack()
-		E2 = Entry(cpara, bd=2)
-		E2.pack()
-
-		L3 = Label(cpara, text="Parameter 3:",bg="white")
-		L3.pack()
-		E3 = Entry(cpara, bd=2)
-		E3.pack()
-
-		L2 = Label(cpara, text="Parameter 4:",bg="white")
-		L2.pack()
-		E2 = Entry(cpara, bd=2)
-		E2.pack()
-
-		button = Button(cpara, text="Submit", command=paraSubmit)
-		button.pack(side = BOTTOM)
-
-	def paraSubmit():
-		tkMessageBox.showinfo("Iris Processing", "Test")
-
-	def searchRecord():
-		tkMessageBox.showinfo("SEARCHING...", "RECORD")
-
-	def aboutProj():
-		aproj = Toplevel(bg="white")
-		aproj.title("Project Goldeneye")
-		aproj.geometry('250x250+0+0')
-
-		message = "Project Members: \nMatt, Joe, Ricky\n\nDetails:\n"
-		Label(aproj,text=message,bg="white").pack(side = TOP)
-
-	######## Creation of the root window #########
-
-	root = Tk()
-	root.title("Healthcare Iris Biometric Scanner")
-
-	Background = PhotoImage(file="images/1.gif")
-	root.geometry("220x220+0+0")
-	panel1 = Label(root,image=Background,bg="black").pack()
-
-	######## Menu Creation #######################
-
-	menubar=Menu(root)
-	filemenu = Menu(menubar,tearoff=0)
-	filemenu.add_command(label="Change Parameters",command=changePara)
-	filemenu.add_separator()
-	filemenu.add_command(label="Quit",command=root.quit)
-
-	menubar.add_cascade(label="File",menu=filemenu)
-
-	helpmenu = Menu(menubar,tearoff=0)
-	helpmenu.add_command(label="About Project",command=aboutProj)
-	menubar.add_cascade(label="Help",menu=helpmenu)
-
-	root.config(menu=menubar)
-
-	######## Button Creation #####################
-
-	b1 = Button(root, text="Load Image", command=loadImage,bg="white")
-	b1.pack(side = LEFT)
-
-	b2 = Button(root, text="Search for Record", command=searchRecord,bg="white")
-	b2.pack(side = RIGHT)
-
-	##############################################
-
-	if len(sys.argv) == 1:
-		w = Label(root, text=" ")
-
-	elif len(sys.argv) == 2:
-		imgPath = sys.argv[1]
-		panelImage = ImageTk.PhotoImage(file=imgPath)
-		w = Label(root, image=panelImage)
-	else:
-		print "Error: Too many arguments"
-
-	w.pack()
-
-	root.mainloop()
-
 
--- goldeni/core/main.py
+++ goldeni/core/main.py
@@ -3,26 +3,27 @@
 import algorithms
 import sys,os,Image
 
-path = sys.argv[1]
-name = os.path.basename(path)
-inputImage = Image.open(path)
+def main(path):
+	name = os.path.basename(path)
+	inputImage = Image.open(path)
 
-grayImageObject = algorithms.grayscaledImage(inputImage)
-grayscaleImage = grayImageObject.grayImage
-grayscaleImage.save("out/gray-" + name)
+	grayImageObject = algorithms.grayscaledImage(inputImage)
+	grayscaleImage = grayImageObject.grayImage
+	grayscaleImage.save("out/gray-" + name)
 
-blurredImageObject = algorithms.blurredImage(grayscaleImage)
-blurredImage = blurredImageObject.blurImage
-blurredImage.save("out/blur-" + name)
+	blurredImageObject = algorithms.blurredImage(grayscaleImage)
+	blurredImage = blurredImageObject.blurImage
+	blurredImage.save("out/blur-" + name)
 
-thresholdedImageObject = algorithms.thresholdedImage(blurredImage,1,1)
-thresholdedImage = thresholdedImageObject.thresholdImage
-#print "Threshold is " + thresholdedImageObject.thr
-thresholdedImage.save("out/thresh-" + name)
+	thresholdedImageObject = algorithms.thresholdedImage(blurredImage,1,1)
+	thresholdedImage = thresholdedImageObject.thresholdImage
+	#print "Threshold is " + thresholdedImageObject.thr
+	thresholdedImage.save("out/thresh-" + name)
+	return thresholdedImage
 
-CannyHoughObject = algorithms.CannyHough(thresholdedImage)
-print CannyHoughObject.cvSize
-print CannyHoughObject.storage
+	#CannyHoughObject = algorithms.CannyHough(thresholdedImage)
+	#print CannyHoughObject.cvSize
+	#print CannyHoughObject.storage
 
 
 
