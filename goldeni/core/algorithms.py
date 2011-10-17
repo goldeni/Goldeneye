12c4c2ee15b1dfe1c33a84367ef9dade3d640d60
goldeni <mtmasset@mtu.edu>
2011-10-17 00:06:12 -0400

    Added gui and other stuff

--- goldeni/core/algorithms.py
+++ goldeni/core/algorithms.py
@@ -27,11 +27,18 @@ class thresholdedImage:
 
 class CannyHough:
 	def __init__(self, inputImage):
-		cvImage = cv.CreateImageHeader(inputImage.size, cv.IPL_DEPTH_8U, 3)
+		cvImage = cv.CreateImageHeader(inputImage.size, cv.IPL_DEPTH_8U, 1)
 		cv.SetData(cvImage, inputImage.tostring())
 		self.cvSize = cv.GetSize(cvImage)
 
-		storage = cv.CreateMat(50, 1, cv.CV_32FC3)
+		cv.SaveImage("out/xyz.jpg",cvImage)
+
+		self.storage = cv.CreateMat(50, 1, cv.CV_32FC3)
 
 		#Fix this shit
-		circles = cv.HoughCircles(cvImage,storage,cv.CV_HOUGH_GRADIENT,2,(self.cvSize[0])/4,200,100);
+		circles = cv.HoughCircles(cvImage,self.storage,cv.CV_HOUGH_GRADIENT,2,(self.cvSize[0])/4,100,50,0,200);
+
+		for (x, y, radius) in circles:
+			print x + "\n"
+			print y + "\n"
+			print radius + "\n"
--- goldeni/core/gui.py
+++ goldeni/core/gui.py
@@ -1,77 +1,123 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
 from Tkinter import *
-import Image,ImageTk,main,tkFileDialog,sys
+import Image,ImageTk,tkFileDialog,sys,tkMessageBox,main
 
-
-###########################################
 ####### Definition buttons CMDs ###########
-###########################################
+
 def globalPath(path):
-	return path
-	
+        return path
+
 def loadImage():
-	imgPath = tkFileDialog.askopenfilename()
-	loadImage = ImageTk.PhotoImage(file=imgPath)
-	print imgPath,loadImage
-	w.loadImage=loadImage
-	w.config(image=loadImage)
+        imgPath = tkFileDialog.askopenfilename()
+        loadImage = ImageTk.PhotoImage(file=imgPath)
+        print imgPath,loadImage
 
-def saveImage():
-	return 0
+	w.destroy()
 
-def compareImage():
-	return 0
+        limage = Toplevel()
+        limage.title("Iris Processing")
+        limage.geometry('250x250+0+0')
 
-def autoProcess(path):
-	newImage = ImageTk.PhotoImage(main.main(path))
-	w.newImage=newImage
-	w.config(image=newImage)
+	newimage = Label(limage, image=loadImage)
+	newimage.loadImage=loadImage
+	newimage.pack()
 
-def manProcess():
-	newImage= ImageTk.PhotoImage(main.main(imgPath))
-	w.newImage=newImage
-	w.config(image=newimage)
+        button = Button(limage, text="Process Image",command=processImage,bg="white")
+	button.pack(side = BOTTOM)
 
+def processImage():
+        tkMessageBox.showinfo("Iris Processing", "Test")
 
-##############################################
+def changePara():
+        cpara = Toplevel(bg="white")
+        cpara.title("Change Parameters")
+        cpara.geometry('250x250+0+0')
+
+        L1 = Label(cpara, text="Hamming Distance:",bg="white")
+        L1.pack()
+        E1 = Entry(cpara, bd=2)
+        E1.pack()
+
+        L2 = Label(cpara, text="Parameter 2:",bg="white")
+        L2.pack()
+        E2 = Entry(cpara, bd=2)
+        E2.pack()
+
+        L3 = Label(cpara, text="Parameter 3:",bg="white")
+        L3.pack()
+        E3 = Entry(cpara, bd=2)
+        E3.pack()
+
+        L2 = Label(cpara, text="Parameter 4:",bg="white")
+        L2.pack()
+        E2 = Entry(cpara, bd=2)
+        E2.pack()
+
+        button = Button(cpara, text="Submit", command=paraSubmit)
+        button.pack(side = BOTTOM)
+
+def paraSubmit():
+        tkMessageBox.showinfo("Iris Processing", "Test")
+
+def searchRecord():
+        tkMessageBox.showinfo("SEARCHING...", "RECORD")
+
+def aboutProj():
+        aproj = Toplevel(bg="white")
+        aproj.title("Project Goldeneye")
+        aproj.geometry('250x250+0+0')
+
+        message = "Project Members: \nMatt, Joe, Ricky\n\nDetails:\n"
+        Label(aproj,text=message,bg="white").pack(side = TOP)
+
 ######## Creation of the root window #########
-##############################################
+
 root = Tk()
+root.title("Healthcare Iris Biometric Scanner")
 
+Background = PhotoImage(file="images/1.gif")
+root.geometry("220x220+0+0")
+panel1 = Label(root,image=Background,bg="black").pack()
 
+######## Menu Creation #######################
+
+menubar=Menu(root)
+filemenu = Menu(menubar,tearoff=0)
+filemenu.add_command(label="Change Parameters",command=changePara)
+filemenu.add_separator()
+filemenu.add_command(label="Quit",command=root.quit)
+
+menubar.add_cascade(label="File",menu=filemenu)
+
+helpmenu = Menu(menubar,tearoff=0)
+helpmenu.add_command(label="About Project",command=aboutProj)
+menubar.add_cascade(label="Help",menu=helpmenu)
+
+root.config(menu=menubar)
+
+######## Button Creation #####################
+
+b1 = Button(root, text="Load Image", command=loadImage,bg="white")
+b1.pack(side = LEFT)
+
+b2 = Button(root, text="Search for Record", command=searchRecord,bg="white")
+b2.pack(side = RIGHT)
+
+##############################################
 
 if len(sys.argv) == 1:
-	w = Label(root, text="Please load an image")
-	
+        w = Label(root, text=" ")
+
 elif len(sys.argv) == 2:
-	imgPath = sys.argv[1]
-	panelImage = ImageTk.PhotoImage(file=imgPath)
-	w = Label(root, image=panelImage)
+        imgPath = sys.argv[1]
+        panelImage = ImageTk.PhotoImage(file=imgPath)
+        w = Label(root, image=panelImage)
 else:
-	print "Error: Too many arguments"
- 
-
-#############################################
-############# Button Creation ###############
-#############################################
-loadButton = Button(root,text="Load Image",command=loadImage)
-saveButton = Button(root,text="Save Image",command=saveImage)
-compareButton = Button(root,text="Compare Image",command=compareImage)
-autoProcessButton = Button(root,text="Auto-Process Image",command=lambda i=imgPath: autoProcess(i))
-manualProcessButton = Button(root,text="Manually Process",command=manProcess)
-
-
-#############################################
-########### Calling Buttons/Image ###########
-#############################################
-saveButton.pack()
-loadButton.pack()
-compareButton.pack()
+        print "Error: Too many arguments"
 
 w.pack()
 
-autoProcessButton.pack()
-manualProcessButton.pack()
-
 root.mainloop()
+
+
