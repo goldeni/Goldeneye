0b086042532a291eff6fde30f35523b3e29437e0
goldeni <mtmasset@mtu.edu>
2011-10-17 06:41:04 -0400

    Merged

--- goldeni/core/gui.py
+++ goldeni/core/gui.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python
 
-<<<<<<< HEAD
-from Tkinter import *
-import Image,ImageTk,tkFileDialog,sys,tkMessageBox,cv
-=======
 import Tkinter
 import Image,ImageTk,tkFileDialog,sys,tkMessageBox
->>>>>>> exp
-import main
 
-####### Definition buttons CMDs ###########
+import main
 
 class mainWindow(Tkinter.Tk):
 	def __init__(self,parent):
@@ -22,61 +16,6 @@ class mainWindow(Tkinter.Tk):
 		cpara.title("Change Parameters")
 		cpara.geometry('250x250+0+0')
 
-<<<<<<< HEAD
-        #limage = Toplevel()
-        #limage.title("Iris Processing")
-        #limage.geometry('400x400+0+0')
-
-	############################################
-	#Test code here
-	b1.destroy()
-	b2.destroy()
-	newimage = Label(root, image=loadImage)
-	newimage.loadImage=loadImage
-	newimage.grid(row=0,column=0,columnspan=2,rowspan=2)
-	root.title("Iris Processing")
-	root.geometry('400x400+0+0')
-	############################################
-
-	#newimage = Label(limage, image=loadImage)
-	#newimage.loadImage=loadImage
-	#newimage.pack()
-
-        button = Button(root, text="Process Image",command=lambda i=imgPath: processImage(i),bg="white")
-	button.grid(row=2,column=0,columnspan=2)
-
-	# This is a really bad practice... but it works so whatever
-	def processImage(imagePath):
-		newimage.destroy()
-		preImage = main.main(imagePath)
-		processedImage = ImageTk.PhotoImage(preImage)
-		newnewimage = Label(root, image=processedImage)
-		newnewimage.processedImage=processedImage
-		newnewimage.grid(row=0,column=0,columnspan=2,rowspan=2)
-=======
-		L1 = Tkinter.Label(cpara, text="Hamming Distance:",bg="white")
-		L1.pack()
-		E1 = Tkinter.Entry(cpara, bd=2)
-		E1.pack()
-
-		L2 = Tkinter.Label(cpara, text="Parameter 2:",bg="white")
-		L2.pack()
-		E2 = Tkinter.Entry(cpara, bd=2)
-		E2.pack()
-
-		L3 = Tkinter.Label(cpara, text="Parameter 3:",bg="white")
-		L3.pack()
-		E3 = Tkinter.Entry(cpara, bd=2)
-		E3.pack()
-
-		L2 = Tkinter.Label(cpara, text="Parameter 4:",bg="white")
-		L2.pack()
-		E2 = Tkinter.Entry(cpara, bd=2)
-		E2.pack()
-
-		button = Tkinter.Button(cpara, text="Submit", command=self.paraSubmit)
-		button.pack(side = BOTTOM)
->>>>>>> exp
 
 	def aboutProj(self):
 		aproj = Tkinter.Toplevel(bg="white")
@@ -118,19 +57,16 @@ class mainWindow(Tkinter.Tk):
 		self.newimage = Tkinter.Label(self, image=loadImage)
 		self.newimage.loadImage=loadImage
 		self.newimage.grid(row=0,column=0,rowspan=2,columnspan=2)
+		
+		self.button = Tkinter.Button(self, text="Close",command=self.quit ,bg="white")
+		self.button.grid(row=2,column=0,columnspan=2)
 
 	def initialize(self):
 		self.grid()
 
-<<<<<<< HEAD
-Background = PhotoImage(file="images/1.gif")
-root.geometry("220x220+0+0")
-#panel1 = Label(root,image=Background,bg="black").pack()
-=======
 		self.backImage = ImageTk.PhotoImage(file="images/1.gif")
 		self.background = Tkinter.Label(self,image=self.backImage,bg="black")
 		self.background.grid(row=0,column=0,columnspan=2,rowspan=2)
->>>>>>> exp
 
 		self.grid_columnconfigure(0,weight=1)
 		self.resizable(True,False)
@@ -152,33 +88,9 @@ root.geometry("220x220+0+0")
 		self.b1 = Tkinter.Button(self, text="Load Image", command=self.loadImage,bg="white")
 		self.b1.grid(row=3,column=0)
 
-<<<<<<< HEAD
-b1 = Button(root, text="Load Image", command=loadImage,bg="white")
-b1.grid(row=0,column=0)
 
-b2 = Button(root, text="Search for Record", command=searchRecord,bg="white")
-b2.grid(row=0,column=1)
-
-##############################################
-
-if len(sys.argv) == 1:
-        w = Label(root, text=" ")
-
-elif len(sys.argv) == 2:
-        imgPath = sys.argv[1]
-        panelImage = ImageTk.PhotoImage(file=imgPath)
-        w = Label(root, image=panelImage)
-else:
-        print "Error: Too many arguments"
-
-w.grid(row=0,column=0)
-
-root.mainloop()
-=======
 		self.b2 = Tkinter.Button(self, text="Search for Record", command=self.searchRecord,bg="white")
 		self.b2.grid(row=3,column=1)
-
->>>>>>> exp
 
 if __name__ == "__main__":
 	root = mainWindow(None)
