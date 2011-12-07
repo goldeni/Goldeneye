d73e355dfdc0729cce7fd3013788dfe7f2602965
Matthew Masseth <mtmasset@mtu.edu>
2011-12-07 18:20:21 -0500

    Regular commit

--- goldeni/core/stats.sh
+++ goldeni/core/stats.sh
@@ -2,7 +2,6 @@
 if [ $1 = "all" ]; then
 	awk -F '\t' '{print $2}' codes.txt	
 else if [ $1 = "1" ]; then
-	
-else if [ $1 = "2" ]; then
-	
+	awk '{ for (i = 1; i <= 9; i++) /00$i/ print $2 }' codes.txt
 fi
+
